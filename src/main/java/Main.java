41e71607a25c06e0045a09b84065cb98d1aed779
cristinag <gomezalonso.cristina@gmail.com>
2026-04-15 09:58:47 +0200

    feat: JPA demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import entity.City;
+import entity.Country;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Country espana = em.find(Country.class,"ESP");
+        City cabral = new City("Cabral", espana, "Galicia", 700);
+        em.persist(cabral);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+
+    }
 }
